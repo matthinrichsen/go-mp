7d9aea20b5028481a913fd8797c1166f2c0c7970
matthinrichsen <hinrichsen.matthew@gmail.com>
2019-03-24 22:36:43 -0500

    Test MP and Pow

--- number_test.go
+++ number_test.go
@@ -48,3 +48,16 @@ func TestNext(t *testing.T) {
 	assert.Equal(t, FromInt64(125), FromInt64(555).Next())
 	assert.Equal(t, FromInt64(4996238671872), FromInt64(277777788888899).Next())
 }
+
+func TestMultiplicationPersistence(t *testing.T) {
+	assert.EqualValues(t, 1, FromInt64(100).MP()) // 100 -> 0
+	assert.EqualValues(t, 2, FromInt64(34).MP())  // 34 -> 12 -> 2
+	assert.EqualValues(t, 3, FromInt64(555).MP()) // 555 -> 125 -> 10 -> 0
+}
+
+func TestPow(t *testing.T) {
+	assert.EqualValues(t, 1, pow(2, 0))
+	assert.EqualValues(t, 2, pow(2, 1))
+	assert.EqualValues(t, 8, pow(2, 3))
+	assert.EqualValues(t, 9, pow(3, 2))
+}
